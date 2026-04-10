c78c01d032f569cb74937c3c906ba86989f0cb23
Joyesh Santra <158501802+Joyesh-Santra@users.noreply.github.com>
2026-04-10 11:42:35 +0530

    ❌ [CodeForge] #1 Hello World — Manual

---
+++ 2026/April/10-Apr/Q1_Hello_World_WRONG_ANSWER_11-42-33.cpp
@@ -0,0 +1,18 @@
+// ═══════════════════════════════════════════════════
+// CodeForge Auto-Sync
+// Problem  : #1 — Hello World
+// Difficulty: Easy
+// Tags     : Basic I/O
+// Submitted: Friday, April 10, 2026 at 11:42:33
+// Verdict  : Manual
+// ❌  WRONG ANSWER — Some test cases failed
+// ═══════════════════════════════════════════════════
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your solution here
+    
+   cout<<"Hello, World!";
+}
